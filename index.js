d4d66c248bd4be525105e48cafaa67c2be172e8e
Chaseaye23 <49164529+Chaseaye23@users.noreply.github.com>
2021-09-20 18:28:13 -0400

    Added command and event handler with a command or two

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_MESSAGE_REACTIONS","GUILD_MESSAGE_TYPING","DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS","DIRECT_MESSAGE_TYPING"] })
 const token = process.env.TOKEN;
 
+bot.commands = new Discord.Collection();
+bot.events = new Discord.Collection();
 
-
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(bot, Discord);
+})
 
 bot.on('ready', () => {
     bot.user.setActivity('Coded By shred#6969', { type: 'WATCHING'})
