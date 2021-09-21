cb35767da74b955e32a49022db4677b50e9cad67
Chaseaye23 <49164529+Chaseaye23@users.noreply.github.com>
2021-09-20 22:32:41 -0400

    Added help , nuke and working embed

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,21 @@
+const { MessageEmbed } = require('discord.js');
 module.exports = {
-    
+    name: 'help',
+    description: 'embeds help messasge',
+     async execute(bot, message, Discord){
+        const newEmbed = new MessageEmbed()
+        .setColor('#7ADBED')
+        .setTitle('Commands')
+        .setDescription('Embed for all of the commands you can use with this bot.')
+        .addFields(
+        {name: 'help', value: "Help command opens this embed."},
+        {name: 'clear', value: "Clears the selected amount of messages"},
+        {name: 'nuke', value: "Nukes a channel."}
+        )
+        .setImage('https://i.ytimg.com/vi/2MCFwDhoqqc/sddefault.jpg')
+        .setFooter('if you read this u hella gay');
+           // console.log(message.channel)
+            message.channel.send({embeds: [newEmbed]});
+       //await message.channel.send({ embeds: newEmbed }); 
+    }
 }
---
+++ commands/nuke.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'nuke',
+    description: "nukes the channel",
+    async execute(client, message, args){
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+            message.channel.bulkDelete(100);
+    }
+}
